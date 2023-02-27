3f140db78c6b4cb81eb53ac0b7a97f63b366726f
na_hyeong <skgud9511@gmail.com>
2023-02-27 20:22:12 +0900

    react- useState 사용해보기

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  // ES6 destructuring 문법
+  // array, object에 있던 자료를 변수에 쉽게 담고 싶을 때
+  let [글제목, 글제목변경] = useState(["코트 추천", "바지 추천", "신발 추천"]); //state 만드는 법
   return (
     <div className="App">
       <div className="navBar">Blog</div>
+      <div className="list">
+        <h4> {글제목[0]} </h4>
+        <p>2월 27일 발행</p>
+        <h4> {글제목[1]} </h4>
+        <p>2월 27일 발행</p>
+        <h4> {글제목[2]} </h4>
+        <p>2월 27일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
