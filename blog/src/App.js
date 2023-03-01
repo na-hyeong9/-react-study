c262e58c556105a53cb0df37fa95edc786721d57
skgud <skgud9511@gmail.com>
2023-03-01 23:19:47 +0900

    230301 react 버튼 기능

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,11 +5,17 @@ function App() {
   // ES6 destructuring 문법
   // array, object에 있던 자료를 변수에 쉽게 담고 싶을 때
   let [글제목, 글제목변경] = useState(["코트 추천", "바지 추천", "신발 추천"]); //state 만드는 법
+  let [좋아요, 좋아요증가] = useState(0);
+  
+  function 함수(){
+    console.log(1);
+  }
+
   return (
     <div className="App">
       <div className="navBar">Blog</div>
       <div className="list">
-        <h4> {글제목[0]} </h4>
+        <h4> {글제목[0]}<span onClick={()=>{ 좋아요증가(좋아요+1) }}>👍</span> {좋아요} </h4>
         <p>2월 27일 발행</p>
         <h4> {글제목[1]} </h4>
         <p>2월 27일 발행</p>
